6f2a719a3ab72e4a0ed8e9464184badbd9521d7a
eujinjung <pinklusha99@kku.ac.kr>
2023-10-22 04:16:38 +0900

    debug

--- detect_server-master/detect.py
+++ detect_server-master/detect.py
@@ -43,5 +43,5 @@ def predict():
 
     return jsonify(predictions=predictions)
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)
-    # app.run(debug=True)
+    # app.run(host='127.0.0.1', port=5000)
+    app.run(debug=True)
